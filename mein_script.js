75bdb5b2de2c2f951159613c2d605f655db3e843
Sinan Yildirim <sinan.yildirim@wibizu.de>
2023-11-20 15:10:11 +0100

    test

---
+++ mein_script.js
@@ -0,0 +1,11 @@
+let x= 42.666;//Hat nur im aktuellen Scope Gültigkeit und ist in diesem Veränderbar
+var y = "Hallo Welt";// Hat überall Gültigkeit und ist in diesem Veränderbar
+const test = {"Erster":4,"Zweiter:":5, "Dritter":18};// hat überall Gültigkeit 
+                                                    // Wertänderung möglich
+
+
+
+//Funktion/Methoden
+
+function hallo(name){
+    alert("Hallo"+name);}
